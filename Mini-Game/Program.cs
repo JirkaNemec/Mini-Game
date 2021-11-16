2cfb32e54101bc5c5b4c7b29b09ca6ac6acfacb8
Jiří Němec <Nemec.Jiri@stud.vassboskovice.cz>
2021-11-16 10:42:27 +0100

    Mini-Game

--- Mini-Game/Program.cs
+++ Mini-Game/Program.cs
@@ -6,7 +6,13 @@ namespace Mini_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int nahCislo = random.Next(1, 100);
+            Console.WriteLine("hádání čísla");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Myslím si číslo od 1 do 100");
+
+            Console.ReadLine();
         }
     }
 }
